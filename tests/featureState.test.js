8e9eb0402336c2d9c97c3bdd019d6263ce15b775
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-07-15 16:22:32 -0700

    added featureState tests

---
+++ tests/featureState.test.js
@@ -0,0 +1,20 @@
+const FeatureState = require("../featureState");
+const Strategy = require("../strategy");
+
+describe("testing feature state", () => {
+  test("creating a new feature state", () => {
+    const featureState = new FeatureState({
+      title: "LOGIN_MICROSERVICE",
+      value: false,
+      strategy: {
+        percentage: 0.1,
+        value: true
+      }
+    });
+    expect(featureState.title).toBe("LOGIN_MICROSERVICE");
+    expect(featureState.value).toBe(false);
+    expect(featureState.strategy).toBeInstanceOf(Strategy);
+    expect(featureState.strategy.percentage).toBe(0.1);
+    expect(featureState.strategy.value).toBe(true);
+  });
+});
