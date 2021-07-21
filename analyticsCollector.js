3d0f3c231a0e9dee52b77f152288fbeb23829da1
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-07-21 08:57:44 -0700

    cleaned up comments from analyticsCollector

--- analyticsCollector.js
+++ analyticsCollector.js
@@ -14,26 +14,6 @@ class AnalyticsCollector {
     this.visitor.event(category, action, label, value, (err) => {
       console.log("Error loggin google analytics event", err);
     });
-    // const trackingId = this.trackingId;
-    // const clientId = this.clientId;
-    // const data = {
-    //   v: '1',
-    //   tid: 'UA-180235909-1',
-    //   cid: '555',
-    //   t: 'event',
-    //   ec: 'test',
-    //   ea: 'print',
-    //   el: 'login',
-    //   ev: '1'
-    // }
-    // const payload = `v=${data.v}&tid=${data.tid}&cid=${data.cid}&t=${data.t}&ec=${data.ec}&ea=${data.ea}&el=${data.el}&ev=${data.ev}`;
-    // console.log(payload);
-    // axios.get('https://www.google-analytics.com/collect?' + encodeURI(payload))
-    // .then(response => {
-    //   console.log(JSON.stringify(response.data));
-    // }).catch((err) => {
-    //   console.log(err);
-    // });
   }
 }
 
